c0da86bfbc3fdf3b36e33e46a5ab44e963c19160
bymarium <marianamejia.91619@gmail.com>
2024-02-23 00:04:51 -0500

    the class and the method to add the numbers of a matrix are created

---
+++ src/exercices/exercise1/SumOfAddition.java
@@ -0,0 +1,22 @@
+package exercices.exercise1;
+
+import exercices.Matrix;
+
+public class SumOfAddition {
+  private class SumOfNumbers {
+    public final Matrix matrix = new Matrix();
+    public int numberAddition(int[][] numbers) {
+      int add = 0;
+
+      for (int i = 0; i < numbers.length; i++) {
+        for (int j = 0; j < numbers.length; j++) {
+          add += numbers[i][j];
+        }
+      }
+
+      matrix.showMatrix(numbers);
+      System.out.println("La suma de los números almacenados de la matriz es: " + add);
+      return add;
+    }
+  }
+}
